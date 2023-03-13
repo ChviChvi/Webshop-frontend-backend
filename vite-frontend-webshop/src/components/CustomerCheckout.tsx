028405f8bef95daf7163ec4ada0b58492b3a3c74
Christiaan Vink <Christiaan_Vink@outlook.com>
2023-03-13 13:22:24 +0100

    API virker, postalcode/city

--- vite-frontend-webshop/src/components/CustomerCheckout.tsx
+++ vite-frontend-webshop/src/components/CustomerCheckout.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import '../stylesheets/Validation.css';
+import '../stylesheets/CustomerCheckout.css'
 
-
+interface CityData {
+    navn: string;
+    postnr: string;
+}
 
 const CustomerCheckout: React.FC = () => {
     const [firstName, setFirstName] = useState('');
@@ -11,7 +15,7 @@ const CustomerCheckout: React.FC = () => {
     const [Address2, setAddress2] = useState('');
     const [postNumber, setpostNumber] = useState('');
     const [City, setCity] = useState('');
-    const [Country, setCountry] = useState('');
+    const [Country, setCountry] = useState('Denmark');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
 
@@ -21,9 +25,43 @@ const CustomerCheckout: React.FC = () => {
         setSubscribe(event.target.checked);
     };
 
+    const handlePostalCodeBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+        const inputValue = event.target.value;
+
+        if (Country === "Denmark") {
+            fetch(`https://api.dataforsyningen.dk/postnumre?nr=${inputValue}`)
+                .then((response) => response.json())
+                .then((data: CityData[]) => {
+                    if (data.length > 0) {
+                        // If the API returns a city, update the state with the city
+                        setCity(data[0].navn);
+                    } else {
+                        // If the API doesn't return a city, clear the input field and city
+                        setCity("");
+                    }
+                })
+                .catch((error) => console.error(error));
+        }
+    };
+
     return (
+        <html>
+
+        <title>
+            WEBSHOP PG2
+        </title>
+
+        <body>
+
+        <div className = "header">
+            <h2>
+                WEBSHOP PG2
+            </h2>
+        </div>
+        <button type="submit"><Link to="/">RETURN TO BASKET</Link></button>
+
         <form>
-            <button type="submit"><Link to="/">RETURN TO BASKET</Link></button>
+
             <div>
                 <input type="text" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder={"Firstname"} pattern="^[\p{L}\s-]+$" required/>
                 <input type="text" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder={"Lastname"} pattern="^[\p{L}\s-]+$" required/>
@@ -37,32 +75,41 @@ const CustomerCheckout: React.FC = () => {
             <div>
                 <input type="text" id="Address2" value={Address2} onChange={(e) => setAddress2(e.target.value)}  placeholder={"Billing Address (Optional)"} pattern="^[\p{L}\s-]+$+[0-9]+$" />
             </div>
-            <div>
-                <input type="text" id="postNumber" value={postNumber} onChange={(e) => setpostNumber(e.target.value)}  placeholder={"PostNumber"} pattern="^[0-9]+$" minLength={4} maxLength={4} required/>
-            </div>
-            <div>
-                <input type="text" id="City" value={City} onChange={(e) => setCity(e.target.value)}  placeholder={"City"} list="Byer" pattern="^[\p{L}\s-]+$" required/>
-                <datalist id="Byer">
-                    <option>København</option>
-                    <option>Aarhus</option>
-                    <option>Odense</option>
-                    <option>Aalborg</option>
-                    <option>Esbjerg</option>
-                    <option>Randers</option>
-                    <option>Kolding</option>
-                    <option>Horsens</option>
-                    <option>Vejle</option>
-                    <option>Roskilde</option>
-                    <option>Farum</option>
-                    <option>Lyngby</option>
-                </datalist>
-            </div>
+
             <div>
                 <input type="text" id="Country" value={Country} onChange={(e) => setCountry(e.target.value)}  placeholder={"Country"} list="Lande" pattern="^[\p{L}\s-]+$" required/>
                 <datalist id="Lande">
                     <option>Denmark</option>
                 </datalist>
             </div>
+
+            <div>
+                <input
+                    type="text"
+                    id="postNumber"
+                    value={postNumber}
+                    onChange={(e) => setpostNumber(e.target.value)}
+                    placeholder={"PostNumber"}
+                    pattern="^[0-9]+$"
+                    minLength={4}
+                    maxLength={4}
+                    required
+                    onBlur={handlePostalCodeBlur}
+                />
+            </div>
+
+            <div>
+                <input
+                    type="text"
+                    id="City"
+                    value={City}
+                    onChange={(e) => setCity(e.target.value)}
+                    placeholder="City"
+                    pattern="^[\p{L}\s-]+$"
+                    required
+                />
+            </div>
+
             <div>
                 <input type="tel" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)}  placeholder={"Phone"} minLength={8} maxLength={8} required/>
             </div>
@@ -75,6 +122,11 @@ const CustomerCheckout: React.FC = () => {
             </div>
 
         </form>
+
+        </body>
+
+
+        </html>
     );
 };
 
