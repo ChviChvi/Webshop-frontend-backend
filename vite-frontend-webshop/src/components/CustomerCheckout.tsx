a5aea5b053e509c3e266add76c01945f335f7b22
Christiaan Vink <Christiaan_Vink@outlook.com>
2023-03-13 09:57:02 +0100

    added some styling and layout, needs major fixing its ugly

--- vite-frontend-webshop/src/components/CustomerCheckout.tsx
+++ vite-frontend-webshop/src/components/CustomerCheckout.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 
-
+import '../stylesheets/CustomerCheckout.css';
 
 const CustomerCheckout: React.FC = () => {
     const [firstName, setFirstName] = useState('');
@@ -20,36 +20,50 @@ const CustomerCheckout: React.FC = () => {
     };
 
     return (
-        <form>
-            <button type="submit"><Link to="/">RETURN TO BASKET</Link></button>
+
             <div>
-                <input type="text" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder={"Firstname"}/>
-                <input type="text" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder={"Lastname"}/>
-            </div>
-            <div>
-                <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder={"E-mail"}/>
-            </div>
-            <div>
-                <input type="text" id="Address" value={Address} onChange={(e) => setAddress(e.target.value)}  placeholder={"Address"}/>
-            </div>
-            <div>
-                <input type="text" id="City" value={City} onChange={(e) => setCity(e.target.value)}  placeholder={"City"}/>
-            </div>
-            <div>
-                <input type="text" id="Country" value={Country} onChange={(e) => setCountry(e.target.value)}  placeholder={"Country"}/>
-            </div>
-            <div>
-                <input type="text" id="postNumber" value={postNumber} onChange={(e) => setpostNumber(e.target.value)}  placeholder={"PostNumber"}/>
-            </div>
-            <div>
-                <input type="tel" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)}  placeholder={"Phone"}/>
-            </div>
-            <div>
-                <label htmlFor="subscribe">Subscribe to our newsletter?</label>
-                <input type="checkbox" id="subscribe" checked={subscribe} onChange={handleSubscribeChange} />
+
+                <div className="header">
+                    <h1>WEBSHOP PG2</h1>
+                </div>
+
+                <div className="links">
+                <button type="submit"><Link to="/">RETURN TO BASKET</Link></button>
+                <button type="submit"><Link to="/checkoutform">Go to checkout</Link></button>
+                </div>
+
+
+
+                <div className="information">
+                    <div className="firstname-box">
+                        <input type="text" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder={"Firstname"}/>
+                        <input type="text" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder={"Lastname"}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder={"E-mail"}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" id="Address" value={Address} onChange={(e) => setAddress(e.target.value)}  placeholder={"Address"}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" id="City" value={City} onChange={(e) => setCity(e.target.value)}  placeholder={"City"}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" id="Country" value={Country} onChange={(e) => setCountry(e.target.value)}  placeholder={"Country"}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" id="postNumber" value={postNumber} onChange={(e) => setpostNumber(e.target.value)}  placeholder={"PostNumber"}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="tel" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)}  placeholder={"Phone"}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="subscribe">Subscribe to our newsletter?</label>
+                        <input type="checkbox" id="subscribe" checked={subscribe} onChange={handleSubscribeChange} />
+                    </div>
+                </div>
             </div>
 
-        </form>
     );
 };
 
