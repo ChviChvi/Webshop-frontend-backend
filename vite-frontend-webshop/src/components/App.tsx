de3adc3959a3fe26ad72f013eff761097d63719e
ChviChvi <christiaan_vink@outlook.com>
2023-04-20 19:38:56 +0200

    adding login feature and started on create account

--- vite-frontend-webshop/src/components/App.tsx
+++ vite-frontend-webshop/src/components/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { checkoutlist } from '../Basket';
 import '../stylesheets/styles.css'; // import CSS styles
-<script src="https://kit.fontawesome.com/yourcode.js" crossOrigin="anonymous"></script>
+//<script src="https://kit.fontawesome.com/yourcode.js" crossOrigin="anonymous"></script>
 import { BrowserRouter as Router,Routes, Route }
     from "react-router-dom";
 
@@ -19,7 +19,6 @@ function App() {
                 <Route path="/" element={<CustomerBasket/>}/>
                 <Route path="/Payment" element={<Payment/>}/>
                 <Route path="/Registration" element={<RegistrationForm/>}/>
-                //TODO nothing happens when you log in
                 <Route path="/Login" element={<Login onSuccess={() => console.log('you have successfully loged in *smileyface*')} />} />
 
 
