fdc2b9c1883361b256883460799ba1fcdf4f270c
Tubsters <Tobias.henriksen2901@gmail.com>
2023-05-01 15:15:30 +0200

    ny test

--- vite-frontend-webshop/Test/CustomerBasket.test.tsx
+++ vite-frontend-webshop/Test/CustomerBasket.test.tsx
@@ -1,9 +1,9 @@
 // @ts-ignore
 
-import { render, screen } from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import userEvent from "@testing-library/user-event";
 import CustomerBasketScroll from "../src/components/CustomerBasketScroll";
+import Payment from "../src/components/Payment";
 
 
 describe(CustomerBasketScroll.name, () => {
@@ -15,9 +15,21 @@ describe(CustomerBasketScroll.name, () => {
         expect(emptyBasketMessage.textContent).toBe("  Your basket is empty, good luck :)");
     });
 
-    it ("remove top item from basket"), () => {
+    it ("Return to basket should take the customer back to the basket", async () => {
         render(<CustomerBasketScroll/>);
+        const { getByText } = render(<Payment />);
+        const returnToBasketButton = getByText('Return to Basket');
+        fireEvent.click(returnToBasketButton);
 
+        expect(window.location.href).toBe('http://localhost:3000/');
     });
 
+    it('Submit Order should submit the form and take the customer back to the home page', async () => {
+        render(<Payment />);
+        const submitOrderButton = screen.getByText('Submit Order');
+        fireEvent.click(submitOrderButton);
+
+        expect(window.location.href).toBe('http://localhost:3000/');
+    });
 });
+
