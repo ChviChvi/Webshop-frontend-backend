b236e361b563ccc11a7748b2abf223ddce51de45
Christiaan Vink <Christiaan_Vink@outlook.com>
2023-05-01 12:42:29 +0200

    fixed routing added the old dom router for reference (commented)

--- vite-frontend-webshop/src/components/App.tsx
+++ vite-frontend-webshop/src/components/App.tsx
@@ -1,17 +1,9 @@
-import { BrowserRouter as Router,Routes, Route }
-    from "react-router-dom";
-
 import CustomerBasket from "./CustomerBasket";
 import CheckoutForm from "./CustomerCheckout";
 import Payment from "./Payment";
 import RegistrationForm from "./UserRegistration";
 import Login from "./Login";
 import Homepage from "./Homepage";
-
-
-
-
-
 
 function App() {
     let page = location.pathname;
@@ -51,3 +43,34 @@ function App() {
 
 export default App;
 
+/**
+ import { BrowserRouter as Router,Routes, Route }
+ from "react-router-dom";
+
+ import CustomerBasket from "./CustomerBasket";
+ import CheckoutForm from "./CustomerCheckout";
+ import Payment from "./Payment";
+ import RegistrationForm from "./UserRegistration";
+ import Login from "./Login";
+ import Homepage from "./Homepage";
+
+
+
+ function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/checkoutform" element={<CheckoutForm/>}/>
+                <Route path="/Basket" element={<CustomerBasket/>}/>
+                <Route path="/Payment" element={<Payment/>}/>
+                <Route path="/Registration" element={<RegistrationForm/>}/>
+                <Route path="/Login"
+                       element={<Login onSuccess={() => console.log('you have successfully loged in *smileyface*')}/>}/>
+                <Route path="/" element={<Homepage/>}/>
+            </Routes>
+        </Router>
+    );
+}
+
+ export default App;
+ */
